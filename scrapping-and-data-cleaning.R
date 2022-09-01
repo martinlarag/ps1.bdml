2f5d7eb1ac3be03940e6ebb784e826e23650bc41
martinlarag <112136440+martinlarag@users.noreply.github.com>
2022-09-01 12:06:05 -0500

    Add files via upload

--- scrapping-and-data-cleaning.R
+++ scrapping-and-data-cleaning.R
@@ -1,48 +1,9 @@
-<<<<<<< Updated upstream
-
-# PS 1 BD&ML --------------------------------------------------------------
-
-## Paula Alarcón
-## Martín Lara
-## Nicolás González
-
-### Punto 1, scrapping y limpieza de datos
-
-
-# Scrapping de datos GEIH -------------------------------------------------
-
-#se cargan los paquetes necesarios
-require(pacman)
-p_load(tidyverse, rvest)
-
-#Se crea un vector con los links de las páginas dónde están los datos
-url_base <- paste0("https://ignaciomsarmiento.github.io/GEIH2018_sample/pages/geih_page_", 1:10, ".html")
-url_base #visualización del vector
-
-#Iterando en cada uno de los urls de la página se descargan los datos  y se juntan en un data frame
-dt <- data.frame()
-for (url in url_base) {
-  print(url)
-  temp <- read_html(url) %>% 
-    html_table() 
-  temp <- as.data.frame(temp[[1]])
-  dt <- rbind(dt, temp)
-}
-dt #el data frame resultante tiene 178 variables y 32177 observaciones
-
-# Limpieza de datos -------------------------------------------------------
-
-dt_mayores <- dt %>% subset(age>=18) #depuramos a todos los menores de edad
-sum(apply(dt_mayores, 1, anyNA))#contamos las filas que contienen NAs
-
-
-=======
 rm(list=ls())
 # PS 1 BD&ML --------------------------------------------------------------
 
 ## Paula Alarcón
 ## Martín Lara
-## Nicolás
+## Nicolás Gonzalez
 
 ### Punto 1, scrapping y limpieza de datos
 
@@ -72,16 +33,20 @@ write.csv(dt, file = "data_ps1.csv")
 
 # Limpieza de datos -------------------------------------------------------
 
-dt_mayores <- dt %>% subset(age>=18) #depuramos a todos los menores de edad
-sum(apply(dt_mayores, 1, anyNA))#contamos las filas que contienen NAs
+dt_emp <- dt %>% subset(age>=18) %>% 
+  subset(dsi==0)#depuramos a todos los menores de edad
 
-na_count <-sapply(dt_mayores, function(y) sum(length(which(is.na(y)))))#creo una función para saber cuantos NAs hay por columna 
-na_cols <- as.data.frame(cbind(colnames(dt_mayores), na_count))#creamos un dataframe con el número de NAs por columna y sus nombres
+sum(apply(dt_emp, 1, anyNA))#contamos las filas que contienen NAs
 
-sum(dt_mayores$iof2, dt_mayores$iof2es)
+na_count <-sapply(dt_emp, function(y) sum(length(which(is.na(y)))))#creo una función para saber cuantos NAs hay por columna 
+na_cols <- as.data.frame(cbind(colnames(dt_emp), na_count))#creamos un dataframe con el número de NAs por columna y sus nombres
 
-dt_sin_jubilados <- dt_mayores %>% subset(iof2==0)#depuramos a las personas qque han recibido ingresos por pensión para mantener solamente personas que reciben sus ingresos por trabajo o subsidios y ayudas
+na_cols <- na_cols %>% mutate(na_count/length(dt_emp))
+
+p_load(heatmaply)
+heatmaply_na(
+  dt_emp[1:30, ],
+  showticklabels = c(TRUE, FALSE)
+)
 
 
-
->>>>>>> Stashed changes
