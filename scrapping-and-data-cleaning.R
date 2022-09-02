7571c69784417048ff072d833fc8c753bcc7ebf4
ngonzalezl1 <112527424+ngonzalezl1@users.noreply.github.com>
2022-09-01 23:45:50 -0500

    Update scrapping-and-data-cleaning.R

--- scrapping-and-data-cleaning.R
+++ scrapping-and-data-cleaning.R
@@ -1,24 +1,35 @@
+#############################################################################
+#
+#              Big Data & Machine Learning for Applied Economics                   
+#                               Problem Set 1
+#                                   Grupo: 
+#                      Paula Alarcón    Código: 201812516
+#                      Martín Lara      Código: 20171...
+#                      Nicolás González Código: 201813698
+#
+#############################################################################
+#
+#                   Punto 1: scrapping y limpieza de datos
+#
+#############################################################################
+
+# Scrapping de datos GEIH ---------------------------------------------------
+
+# Limpio mi lugar de trabajo
 rm(list=ls())
-# PS 1 BD&ML --------------------------------------------------------------
 
-## Paula Alarcón
-## Martín Lara
-## Nicolás Gonzalez
-
-### Punto 1, scrapping y limpieza de datos
-
-
-# Scrapping de datos GEIH -------------------------------------------------
+# Asigno un directorio donde me guarde los resultados: 
+setwd(choose.dir())
 
 #se cargan los paquetes necesarios
 require(pacman)
-p_load(tidyverse, rvest)
+p_load(tidyverse, rvest, heatmaply, plyr, dplyr, stargazer)
 
 #Se crea un vector con los links de las páginas dónde están los datos
 url_base <- paste0("https://ignaciomsarmiento.github.io/GEIH2018_sample/pages/geih_page_", 1:10, ".html")
 url_base #visualización del vector
 
-#Iterando en cada uno de los urls de la página se descargan los datos  y se juntan en un data frame
+#Iterando en cada uno de las urls de la página se descargan los datos y se juntan en un data frame
 dt <- data.frame()
 for (url in url_base) {
   print(url)
@@ -27,26 +38,70 @@ for (url in url_base) {
   temp <- as.data.frame(temp[[1]])
   dt <- rbind(dt, temp)
 }
-dt #el data frame resultante tiene 178 variables y 32177 observaciones
+View(dt) #el data frame resultante tiene 178 variables y 32177 observaciones
 
 write.csv(dt, file = "data_ps1.csv")
 
-# Limpieza de datos -------------------------------------------------------
+# Limpieza de datos ------------------------------------------------------------
 
 dt_emp <- dt %>% subset(age>=18) %>% 
   subset(dsi==0)#depuramos a todos los menores de edad
 
-sum(apply(dt_emp, 1, anyNA))#contamos las filas que contienen NAs
+#Sacamos el porcentaje de missing values por variable
+na_percentage <-sapply(dt, function(y) sum(length(which(is.na(y))))/length(dt$directorio))#creo una función para saber cuantos NAs hay por columna 
+data_x <- as.data.frame(na_percentage)
 
-na_count <-sapply(dt_emp, function(y) sum(length(which(is.na(y)))))#creo una función para saber cuantos NAs hay por columna 
-na_cols <- as.data.frame(cbind(colnames(dt_emp), na_count))#creamos un dataframe con el número de NAs por columna y sus nombres
+# En un priomer proceso de eliminación de variables, eliminamos aquellas con un 
+#alto porcentaje de missing values
+var <- cbind(Var_name = rownames(data_x), data_x)
+rownames(var) <- 1:nrow(var)
+var_for_drop <- var[var$na_percentage>=0.45,]
+var_for_keep <- var[var$na_percentage<0.45,]
+count(var) # Contamos cuantas variables tenemos en total (=178)
+count(var_for_keep) # Contamos cuantas variables tienen % missing menor o igual a 45% (=48)
+count(var_for_drop) # Contamos cuantas variables tienen % missing mayor a 45% (=130)
 
-na_cols <- na_cols %>% mutate(na_count/length(dt_emp))
+# Elimino de mi base de datos aquellas variables con alto porcentaje de NAS
+var_drop <- list(var_for_drop$Var_name)
+# Acá hice una lista con mis nombre de variables quer quiero dropear pero no me deja HELP
+unlist(var_drop, recursive = TRUE, use.names = TRUE)
+dt_new <- dt %>% select(- one_of(c(as.character(var_drop))))
 
-p_load(heatmaply)
-heatmaply_na(
-  dt_emp[1:30, ],
-  showticklabels = c(TRUE, FALSE)
-)
+# Estadisticas descriptivas ------------------------------------------------------------
+stargazer(dt, type='text')
+
+dt_income <- dt %>% select(directorio,estrato1,sex,age,ingtot,college,ingtot,ingtotes,ingtotob,cuentaPropia,totalHoursWorked)
+
+
+# carga de library VIM para obtener función kNN ----------------------------------------
+library(VIM)
+newdata <-  kNN(airquality, variable = c("Ozone", "Solar.R"), k = 6)
+summary(newdata)
+
+#############################################################################
+#
+#              Big Data & Machine Learning for Applied Economics                   
+#                               Problem Set 1
+#                                   Grupo: 
+#                      Paula Alarcón    Código: 201812516
+#                      Martín Lara      Código: 20171...
+#                      Nicolás González Código: 201813698
+#
+#############################################################################
+#
+#                      Punto 2: Age-Earnings Profile
+#
+#############################################################################
+
+# 1) Variables relevantes ---------------------------------------------------
+dt_earnings <- dt %>% select(directorio,estrato1,sex,age,ingtot,college,ingtot,ingtotes,ingtotob,cuentaPropia,totalHoursWorked)
+
+# Creo una nueva variable de edad al cuadrado:
+
+dt_earnings <- dt_earnings %>%
+  mutate(
+    age2 = age**2
+  )
+#
 
 
