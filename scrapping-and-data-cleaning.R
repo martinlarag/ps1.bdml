74c539b753aa0301d742a247d9ff708ecaec312e
paalarcon <112449325+paalarcon@users.noreply.github.com>
2022-09-04 22:17:02 -0500

    Update scrapping-and-data-cleaning.R

--- scrapping-and-data-cleaning.R
+++ scrapping-and-data-cleaning.R
@@ -41,44 +41,7 @@ for (url in url_base) {
 View(dt) #el data frame resultante tiene 178 variables y 32177 observaciones
 
 write.csv(dt, file = "data_ps1.csv")
-
-# Limpieza de datos ------------------------------------------------------------
-
-dt_emp <- dt %>% subset(age>=18) %>% 
-  subset(dsi==0)#depuramos a todos los menores de edad
-
-#Sacamos el porcentaje de missing values por variable
-na_percentage <-sapply(dt, function(y) sum(length(which(is.na(y))))/length(dt$directorio))#creo una función para saber cuantos NAs hay por columna 
-data_x <- as.data.frame(na_percentage)
-
-# En un priomer proceso de eliminación de variables, eliminamos aquellas con un 
-#alto porcentaje de missing values
-var <- cbind(Var_name = rownames(data_x), data_x)
-rownames(var) <- 1:nrow(var)
-var_for_drop <- var[var$na_percentage>=0.45,]
-var_for_keep <- var[var$na_percentage<0.45,]
-count(var) # Contamos cuantas variables tenemos en total (=178)
-count(var_for_keep) # Contamos cuantas variables tienen % missing menor o igual a 45% (=48)
-count(var_for_drop) # Contamos cuantas variables tienen % missing mayor a 45% (=130)
-
-# Elimino de mi base de datos aquellas variables con alto porcentaje de NAS
-var_drop <- list(var_for_drop$Var_name)
-# Acá hice una lista con mis nombre de variables quer quiero dropear pero no me deja HELP
-unlist(var_drop, recursive = TRUE, use.names = TRUE)
-dt_new <- dt %>% select(- one_of(c(as.character(var_drop))))
-
-# Estadisticas descriptivas ------------------------------------------------------------
-stargazer(dt, type='text')
-
-dt_income <- dt %>% select(directorio,estrato1,sex,age,ingtot,college,ingtot,ingtotes,ingtotob,cuentaPropia,totalHoursWorked)
-
-
-# carga de library VIM para obtener función kNN ----------------------------------------
-library(VIM)
-newdata <-  kNN(airquality, variable = c("Ozone", "Solar.R"), k = 6)
-summary(newdata)
-
-############ NUEVO PUNTO 1 ###############                       
+                    
 # Limpieza de datos ------------------------------------------------------------
 #Creamos una base con las variables de interés
 
